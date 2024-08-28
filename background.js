5804f3f2cbdd3e933e9fb15f98e817c690c7e450
edersonbadeca <edersonbadeca@users.noreply.github.com>
2024-08-28 11:21:48 -0300

    update manisfest

--- background.js
+++ background.js
@@ -1,3 +1,32 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed');
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "highlight-text") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: () => {
+          let selectedText = window.getSelection().toString();
+          if (selectedText.length > 0) {
+            let span = document.createElement('span');
+            span.style.backgroundColor = 'yellow';
+            span.className = 'highlighted';
+            let range = window.getSelection().getRangeAt(0);
+            range.surroundContents(span);
+            window.getSelection().removeAllRanges();
+
+            // Salvar o highlight no storage
+            saveHighlight(selectedText);
+          }
+
+          function saveHighlight(text) {
+            const url = window.location.href;
+            chrome.storage.local.get({ highlights: [] }, function (result) {
+              let highlights = result.highlights;
+              highlights.push({ text: text, url: url });
+              chrome.storage.local.set({ highlights: highlights });
+            });
+          }
+        }
+      });
+    });
+  }
 });
--- content.js
+++ content.js
@@ -1,21 +1,17 @@
-function highlightSelection() {
-  document.addEventListener('mouseup', function () {
-    let selectedText = window.getSelection().toString();
-    if (selectedText.length > 0) {
-      let span = document.createElement('span');
-      span.style.backgroundColor = 'yellow';
-      span.className = 'highlighted';
-      let range = window.getSelection().getRangeAt(0);
-      range.surroundContents(span);
-      window.getSelection().removeAllRanges();
+function highlightSelectedText() {
+  let selectedText = window.getSelection().toString();
+  if (selectedText.length > 0) {
+    let span = document.createElement('span');
+    span.style.backgroundColor = 'yellow';
+    span.className = 'highlighted';
+    let range = window.getSelection().getRangeAt(0);
+    range.surroundContents(span);
+    window.getSelection().removeAllRanges();
 
-      // Salvar o highlight no storage
-      saveHighlight(selectedText);
-    }
-  });
+    // Salvar o highlight no storage
+    saveHighlight(selectedText);
+  }
 }
-
-highlightSelection();
 
 function saveHighlight(text) {
   const url = window.location.href;
